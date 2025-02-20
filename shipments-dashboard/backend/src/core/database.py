e7351b6c85c517c60986dfcec360faad83669f15
Gavin Fung <gavin@sokanu.com>
2025-02-20 13:30:02 -0800

    fix docker setup

--- shipments-dashboard/backend/src/core/database.py
+++ shipments-dashboard/backend/src/core/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.exc import OperationalError
 import os
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,9 +10,22 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # SQLAlchemy database setup
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+for attempt in range(5):
+    try:
+        engine = create_engine(DATABASE_URL)
+        SessionLocal = sessionmaker(
+            autocommit=False, autoflush=False, bind=engine)
+        print("Successfully connected to the database.")
+        break
+    except OperationalError:
+        print(f" Database not ready, retrying ({attempt+1}/5)...")
+        time.sleep(3)
+else:
+    raise Exception("Database connection failed after 5 retries.")
+
+
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
@@ -18,4 +33,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
